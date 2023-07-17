ec30c745426368e4f738f9681742e45719900717
Ajay Kumar Devaraju <ADevaraju@ESTUATE.COM>
2023-07-17 15:59:05 +0530

    Validating datasource, View and Download API, Exception Handling, Encrypting username and password

--- src/main/java/com/archlens/controller/ArchLensController.java
+++ src/main/java/com/archlens/controller/ArchLensController.java
@@ -28,10 +28,10 @@ import com.archlens.service.ArchLensService;
 @RequestMapping("/ArchLens")
 public class ArchLensController {
 
-	
-	
-	
-	@PostMapping("/addConfig")
+
+
+
+	@PostMapping("/data-source")
 	public ResponseEntity<?> addConfig(@RequestBody ExternalTableDataSource configData) {
 		try {
 			String respone =  ArchLensService.addConfig(configData);
@@ -43,53 +43,6 @@ public class ArchLensController {
 		} 
 
 	}
-
-	@GetMapping(value = "/view/ds={config}/schema={schema}/tablename={table}/blobColName={blobColName}/fileName={fileName}/{idName}={idVal}", produces = MediaType.ALL_VALUE)
-	public ResponseEntity<?> viewBlob(@PathVariable String config, @PathVariable String schema,
-			@PathVariable String table, @PathVariable String blobColName, @PathVariable String fileName,
-			@PathVariable String idName, @PathVariable String idVal, HttpServletResponse response) {
-
-		try {
-			ArchLensService.viewBlobData(config, schema, table, blobColName, fileName, idName, idVal, response);
-
-			return new ResponseEntity<String>("Details Fetched Sucesfully", HttpStatus.CREATED);
-		} catch (SQLException c) {
-			return new ResponseEntity<String>(c.getMessage(), HttpStatus.BAD_REQUEST);
-		} catch (IOException p) {
-			return new ResponseEntity<String>(p.getMessage(), HttpStatus.BAD_REQUEST);
-		}
-
-	}
-	
-	
-	@GetMapping(value = "/download/ds={config}/schema={schema}/tablename={table}/blobColName={blobColName}/fileName={file_name}/{idName}={idVal}", produces = MediaType.ALL_VALUE)
-	public void downloadFile(@PathVariable String config, @PathVariable String schema, @PathVariable String table,
-			@PathVariable String blobColName, @PathVariable String file_name, @PathVariable String idName,
-			@PathVariable String idVal, HttpServletResponse response) throws IOException, SQLException {
-
-		List result = ExternalTableConfig.createConnection(config, schema, table, blobColName, file_name, idName,idVal);
-
-		Object fileData = result.get(0);
-		String fileName = (String) result.get(1);
-
-		byte[] content = (byte[]) fileData;
-		if (fileData != null) {
-			// Set response headers
-			response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
-			response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
-
-			// Write the content directly to the response output stream
-			OutputStream outputStream = response.getOutputStream();
-			outputStream.write(content);
-			outputStream.flush();
-			outputStream.close();
-		} else {
-			// File not found in the database
-			response.sendError(HttpServletResponse.SC_NOT_FOUND);
-		}
-		
-	}
-
 
 	@GetMapping("/ds={datasource}/schemas")
 	public List getSchemas(@PathVariable String datasource) {
@@ -114,5 +67,54 @@ public class ArchLensController {
 		}
 		return schemas;
 	}
+
+	@GetMapping(value = "/view/ds={datasource}/schema={schema}/tablename={table}/blobColName={blobColName}/fileName={fileName}/{idName}={idVal}", produces = MediaType.ALL_VALUE)
+	public ResponseEntity<?> viewBlob(@PathVariable String datasource, @PathVariable String schema,
+			@PathVariable String table, @PathVariable String blobColName, @PathVariable String fileName,
+			@PathVariable String idName, @PathVariable String idVal, HttpServletResponse response) {
+
+		try {
+			ArchLensService.viewBlobData(datasource, schema, table, blobColName, fileName, idName, idVal, response);
+
+			return new ResponseEntity<String>("Details Fetched Sucesfully", HttpStatus.CREATED);
+		} catch (SQLException c) {
+			return new ResponseEntity<String>(c.getMessage(), HttpStatus.BAD_REQUEST);
+		} catch (IOException p) {
+			return new ResponseEntity<String>(p.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+
+	}
+
+
+	@GetMapping(value = "/download/ds={datasource}/schema={schema}/tablename={table}/blobColName={blobColName}/fileName={file_name}/{idName}={idVal}", produces = MediaType.ALL_VALUE)
+	public void downloadFile(@PathVariable String datasource, @PathVariable String schema, @PathVariable String table,
+			@PathVariable String blobColName, @PathVariable String file_name, @PathVariable String idName,
+			@PathVariable String idVal, HttpServletResponse response) throws IOException, SQLException {
+
+		List result = ExternalTableConfig.createConnection(datasource, schema, table, blobColName, file_name, idName,idVal);
+
+		Object fileData = result.get(0);
+		String fileName = (String) result.get(1);
+
+		byte[] content = (byte[]) fileData;
+		if (fileData != null) {
+			// Set response headers
+			response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+			response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+
+			// Write the content directly to the response output stream
+			OutputStream outputStream = response.getOutputStream();
+			outputStream.write(content);
+			outputStream.flush();
+			outputStream.close();
+		} else {
+			// File not found in the database
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+		}
+
+	}
+
+
+
 
 }
